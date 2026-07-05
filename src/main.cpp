d73b84edde0615dff3f6cf9b5d7d357bea80d42c
livemehere <rhdxoals@gmail.com>
2026-07-05 17:41:43 +0900

    apply dpr

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,19 @@
 #include "SDL3/SDL_main.h"
 #include "SDL3_ttf/SDL_ttf.h"
 
-void DrawText(SDL_Renderer *renderer, TTF_Font *font, const std::string &text, const SDL_Point pos, SDL_Color color) {
+void DrawText(SDL_Renderer *renderer, TTF_Font *font, const float dpr, const std::string &text, const SDL_Point &pos,
+              const SDL_Color &color) {
     SDL_Surface *surface = TTF_RenderText_Blended(
         font,
         text.c_str(),
         0,
         color);
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FRect dst{
-        (float) pos.x,
-        (float) pos.y,
-        (float) surface->w,
-        (float) surface->h
+    const SDL_FRect dst{
+        static_cast<float>(pos.x) * dpr,
+        static_cast<float>(pos.y) * dpr,
+        static_cast<float>(surface->w),
+        static_cast<float>(surface->h)
     };
     SDL_RenderTexture(renderer, texture, nullptr, &dst);
 }
@@ -26,13 +27,22 @@ int main() {
     }
 
     SDL_Window *window = SDL_CreateWindow(
-        "TCanvas", 1280, 720, 0);
+        "TCanvas", 1280, 720, SDL_WINDOW_HIGH_PIXEL_DENSITY);
 
     if (!window) {
         SDL_Log("Creating Window failed: %s", SDL_GetError());
         SDL_Quit();
         return 1;
     }
+
+    // DPR --
+    int ww, wh;
+    int pw, ph;
+    SDL_GetWindowSize(window, &ww, &wh);
+    SDL_GetWindowSizeInPixels(window, &pw, &ph);
+    const float dpr = static_cast<float>(pw) / static_cast<float>(ww);
+    // DPR --
+
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
     if (!renderer) {
@@ -43,7 +53,7 @@ int main() {
     }
 
 
-    TTF_Font *font = TTF_OpenFont("/System/Library/Fonts/Supplemental/Arial.ttf", 16);
+    TTF_Font *font = TTF_OpenFont("/System/Library/Fonts/Supplemental/Arial.ttf", 16 * dpr);
     if (!font) {
         SDL_Log("Font loading failed: %s", SDL_GetError());
         return 1;
@@ -66,13 +76,14 @@ int main() {
 
         // Rect
         SDL_FRect rect{
-            10.0f, 10.0f, 100.0f, 100.0f
+            10.0f * dpr, 10.0f * dpr, 100.0f * dpr, 100.0f * dpr
         };
         SDL_SetRenderDrawColor(renderer, 80, 180, 255, 255);
         SDL_RenderFillRect(renderer, &rect);
 
         // Text
-        DrawText(renderer, font, "Hello world", {10, 10}, {255, 255, 255, 255});
+        DrawText(renderer, font, dpr, "Hello world", {10, 10}, {255, 255, 255, 255});
+        DrawText(renderer, font, dpr, "Canvas", {200, 10}, {255, 255, 255, 255});
 
         // end --
 
