22f081647a4a8b4664fc666bc97b15b0ac0bbdb0
livemehere <rhdxoals@gmail.com>
2026-07-05 17:00:58 +0900

    setup sdl loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
 
+#include "SDL3/SDL.h"
+#include "SDL3/SDL_main.h"
+
 int main() {
-    std::cout << "Hello world" << std::endl;
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "TCanvas", 1280, 720, 0);
+
+    if (!window) {
+        SDL_Log("Creating Window failed: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer) {
+        SDL_Log("Creating Renderer failed: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // begin --
+        SDL_FRect rect{
+            10.0f, 10.0f, 100.0f, 100.0f
+        };
+        SDL_SetRenderDrawColor(renderer, 80, 180, 255, 255);
+        SDL_RenderFillRect(renderer, &rect);
+        // end --
+
+        SDL_RenderPresent(renderer);
+    }
+
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
