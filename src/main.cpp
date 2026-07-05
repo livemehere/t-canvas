03be66845382e65108840b5123ff92054ee830e3
livemehere <rhdxoals@gmail.com>
2026-07-06 03:59:02 +0900

    skia setup done

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,23 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+#include <skia/core/SkCanvas.h>
+#include <skia/core/SkColorSpace.h>
+#include <skia/core/SkPaint.h>
+#include <skia/core/SkRect.h>
+#include <skia/core/SkSurface.h>
+
+#include <skia/gpu/ganesh/GrBackendSurface.h>
+#include <skia/gpu/ganesh/GrDirectContext.h>
+#include <skia/gpu/ganesh/SkSurfaceGanesh.h>
+#include <skia/gpu/ganesh/gl/GrGLBackendSurface.h>
+#include <skia/gpu/ganesh/gl/GrGLDirectContext.h>
+#include <skia/gpu/ganesh/gl/GrGLInterface.h>
+
 namespace {
-void GlfwErrorCallback(int error, const char *description) {
-    std::fprintf(stderr, "GLFW error %d: %s\n", error, description);
-}
+    void GlfwErrorCallback(int error, const char *description) {
+        std::fprintf(stderr, "GLFW error %d: %s\n", error, description);
+    }
 } // namespace
 
 int main() {
@@ -34,6 +47,10 @@ int main() {
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
+    // skia
+    auto glInterface = GrGLMakeNativeInterface();
+    auto grContext = GrDirectContexts::MakeGL(glInterface);
+
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
 
@@ -56,6 +73,38 @@ int main() {
         glClearColor(0.02f, 0.02f, 0.025f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // skia
+        GrGLFramebufferInfo framebufferInfo;
+        framebufferInfo.fFBOID = 0;
+        framebufferInfo.fFormat = GL_RGBA8;
+        GrBackendRenderTarget renderTarget = GrBackendRenderTargets::MakeGL(
+            framebufferWidth,
+            framebufferHeight,
+            0,
+            8,
+            framebufferInfo
+        );
+        auto surface = SkSurfaces::WrapBackendRenderTarget(
+            grContext.get(),
+            renderTarget,
+            kBottomLeft_GrSurfaceOrigin,
+            kRGBA_8888_SkColorType,
+            nullptr,
+            nullptr
+        );
+        SkCanvas *canvas = surface->getCanvas();
+        canvas->clear(SK_ColorBLACK);
+        canvas->save();
+        SkPaint paint;
+        paint.setAntiAlias(true);
+        paint.setColor(SkColorSetARGB(255, 80, 180, 255));
+        canvas->drawRect(
+            SkRect::MakeXYWH(80, 80, 200, 100),
+            paint
+        );
+        canvas->restore();
+        grContext->flushAndSubmit(surface.get());
+
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
@@ -63,7 +112,7 @@ int main() {
         ImDrawList *drawList = ImGui::GetForegroundDrawList();
         drawList->AddRectFilled(
             ImVec2(80.0f, 80.0f),
-            ImVec2(280.0f, 200.0f),
+            ImVec2(280.0f, 180.0f),
             IM_COL32(80, 180, 255, 255),
             6.0f);
 
